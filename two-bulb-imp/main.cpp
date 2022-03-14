3a871bf7f51e21061056642db5bb5a23402db223
derek <harrison.derek.w@gmail.com>
2022-03-14 20:41:12 +0100

    cleanup

--- two-bulb-imp/main.cpp
+++ two-bulb-imp/main.cpp
@@ -27,6 +27,21 @@ typedef struct bulb_data {
     node_t mol_fracs_bulb2;
 } b_data_t;
 
+typedef struct time_parameters {
+    double to;
+    double tf;
+    double dt;
+    int nt;
+} t_params_t;
+
+typedef struct experiment_params {
+    double V;
+    double d;
+    double len;
+    double A;
+    double dz;
+} e_params_t;
+
 double a1(p_params_t & p_params, double x2) {
     double v1 = 1.0 / p_params.D13;
     double v2 = 1.0 / p_params.D12;
@@ -94,43 +109,15 @@ double beta2(p_params_t & p_params, double x1, double x2) {
     return - a2_loc * alpha2_loc / a1_loc;
 }
 
-int main(int argc, const char * argv[]) {
+void compute_bulb_compositions(e_params_t e_params,
+                               p_params_t p_params,
+                               t_params_t t_params,
+                               int ng,
+                               b_data_t & bulb_data) {
     
-    // Number of grid nodes
-    int ng = 10;
-    
-    // Experimental setup parameters
-    double V = 5e-4;
-    double d = 2e-3;
-    double len = 1e-2;
-    double A = 0.25 * 3.14 * d * d;
-    double dz = len / ng;
-    
-    // Initial composition bulb 1
-    b_data_t bulb_data;
-    bulb_data.mol_fracs_bulb1.x1 = 0.501;
-    bulb_data.mol_fracs_bulb1.x2 = 0.499;
-    bulb_data.mol_fracs_bulb1.x3 = 1 - 0.501 - 0.499;
-    
-    // Initial composition bulb 2
-    bulb_data.mol_fracs_bulb2.x1 = 0.0;
-    bulb_data.mol_fracs_bulb2.x2 = 0.501;
-    bulb_data.mol_fracs_bulb2.x3 = 1 - 0.0 - 0.501;
-    
-    // Total concentration
-    p_params_t p_params;
-    p_params.ct = 1.0;
-    
-    // Time parameters
-    double to = 0.0;
-    double tf = 20.0;
-    int nt = 40;
-    double dt = (double) (tf - to) / nt;
-    
-    // Diffusivities
-    p_params.D12 = 8.33e-5 * 3600;
-    p_params.D13 = 6.8e-5 * 3600;
-    p_params.D23 = 1.68e-5 * 3600;
+    double V = e_params.V;
+    double A = e_params.A;
+    double dz = e_params.dz;
     
     // Mole fractions in tube
     node_t * tube_fracs = new node_t[ng];
@@ -155,12 +142,13 @@ int main(int argc, const char * argv[]) {
     }
     
     // Perform iterations
-    int max_out_it = 500;
+    int max_out_it = 300;
     int max_in_it = 300;
     
-    double t = to;
+    double t = t_params.to;
+    double dt = (t_params.tf - t_params.to) / t_params.nt;
     
-    while(t < tf) {
+    while(t < t_params.tf) {
         
         bulb_data_old = bulb_data;
         
@@ -406,119 +394,163 @@ int main(int argc, const char * argv[]) {
         
         t = t + dt;
     }
+}
+
+int main(int argc, const char * argv[]) {
     
-    // Check Bulb 1 computation
-    double x1_loc = bulb_data.mol_fracs_bulb1.x1;
-    double x2_loc = bulb_data.mol_fracs_bulb1.x2;
-    double a1_loc = alpha1(p_params, x1_loc, x2_loc);
-    double a2_loc = alpha2(p_params, x1_loc, x2_loc);
-    double b1_loc = beta1(p_params, x1_loc, x2_loc);
-    double b2_loc = beta2(p_params, x1_loc, x2_loc);
+    // Number of grid nodes
+    int ng = 10;
     
-    double x1P = tube_fracs[0].x1;
-    double x1W = x1_loc;
+    // Experimental setup parameters
+    e_params_t e_params;
+    e_params.V = 5e-4;
+    e_params.d = 2e-3;
+    e_params.len = 1e-2;
+    e_params.A = 0.25 * 3.14 * e_params.d * e_params.d;
+    e_params.dz = e_params.len / ng;
     
-    double x2P = tube_fracs[0].x2;
-    double x2W = x2_loc;
+    // Initial composition bulb 1
+    b_data_t bulb_data;
+    bulb_data.mol_fracs_bulb1.x1 = 0.501;
+    bulb_data.mol_fracs_bulb1.x2 = 0.499;
+    bulb_data.mol_fracs_bulb1.x3 = 1 - 0.501 - 0.499;
     
-    double J1_0 = b1_loc * (x1P - x1W) / (0.5 * dz) + b2_loc * (x2P - x2W) / (0.5 * dz);
-    double J2_0 = a1_loc * (x1P - x1W) / (0.5 * dz) + a2_loc * (x2P - x2W) / (0.5 * dz);
+    // Initial composition bulb 2
+    bulb_data.mol_fracs_bulb2.x1 = 0.0;
+    bulb_data.mol_fracs_bulb2.x2 = 0.501;
+    bulb_data.mol_fracs_bulb2.x3 = 1 - 0.0 - 0.501;
     
-    double var1c1 = - J1_0 * A;
-    double var2c1 = V * p_params.ct / dt * (bulb_data.mol_fracs_bulb1.x1 - bulb_data_old.mol_fracs_bulb1.x1);
+    // Total concentration
+    p_params_t p_params;
+    p_params.ct = 1.0;
     
-    double var1c2 = - J2_0 * A;
-    double var2c2 = V * p_params.ct / dt * (bulb_data.mol_fracs_bulb1.x2 - bulb_data_old.mol_fracs_bulb1.x2);
+    // Time parameters
+    t_params_t t_params;
+    t_params.to = 0.0;
+    t_params.tf = 5.0;
+    t_params.nt = 10;
+    t_params.dt = (double) (t_params.tf - t_params.to) / t_params.nt;
     
-    double ratio1 = var1c1 / var2c1;
-    double ratio2 = var1c2 / var2c2;
+    // Diffusivities
+    p_params.D12 = 8.33e-5 * 3600;
+    p_params.D13 = 6.8e-5 * 3600;
+    p_params.D23 = 1.68e-5 * 3600;
+
     
-    std::cout << "ratio 1 bulb 1: " << ratio1 << std::endl;
-    std::cout << "ratio 2 bulb 1: " << ratio2 << std::endl;
+    compute_bulb_compositions(e_params, p_params, t_params, ng, bulb_data);
     
-    // Check first node
-    
-    // Check mid tube nodes
-    for(int node = 1; node < ng - 1; ++node) {
-        double x1_loc_z = 0.5 * (tube_fracs[node - 1].x1 + tube_fracs[node].x1);
-        double x2_loc_z = 0.5 * (tube_fracs[node - 1].x2 + tube_fracs[node].x2);
-        double a1_loc_z = alpha1(p_params, x1_loc_z, x2_loc_z);
-        double a2_loc_z = alpha2(p_params, x1_loc_z, x2_loc_z);
-        double b1_loc_z = beta1(p_params, x1_loc_z, x2_loc_z);
-        double b2_loc_z = beta2(p_params, x1_loc_z, x2_loc_z);
-     
-        double x1_loc_z_dz = 0.5 * (tube_fracs[node + 1].x1 + tube_fracs[node].x1);
-        double x2_loc_z_dz = 0.5 * (tube_fracs[node + 1].x2 + tube_fracs[node].x2);
-        double a1_loc_z_dz = alpha1(p_params, x1_loc_z_dz, x2_loc_z_dz);
-        double a2_loc_z_dz = alpha2(p_params, x1_loc_z_dz, x2_loc_z_dz);
-        double b1_loc_z_dz = beta1(p_params, x1_loc_z_dz, x2_loc_z_dz);
-        double b2_loc_z_dz = beta2(p_params, x1_loc_z_dz, x2_loc_z_dz);
-        
-        double x1P = tube_fracs[node].x1;
-        double x1W = tube_fracs[node - 1].x1;
-        double x1E = tube_fracs[node + 1].x1;
-        
-        double x2P = tube_fracs[node].x2;
-        double x2W = tube_fracs[node - 1].x2;
-        double x2E = tube_fracs[node + 1].x2;
-        
-        double J1_z = b1_loc_z * (x1P - x1W) / dz + b2_loc_z * (x2P - x2W) / dz;
-        double J1_z_dz = b1_loc_z_dz * (x1E - x1P) / dz + b2_loc_z_dz * (x2E - x2P) / dz;
-        double J2_z = a1_loc_z * (x1P - x1W) / dz + a2_loc_z * (x2P - x2W) / dz;
-        double J2_z_dz = a1_loc_z_dz * (x1E - x1P) / dz + a2_loc_z_dz * (x2E - x2P) / dz;
-        
-        double var1c1 = p_params.ct * (tube_fracs[node].x1 - tube_fracs_old[node].x1) / dt;
-        double var2c1 = (J1_z - J1_z_dz) / dz;
-        
-        double var1c2 = p_params.ct * (tube_fracs[node].x2 - tube_fracs_old[node].x2) / dt;
-        double var2c2 = (J2_z - J2_z_dz) / dz;
-        
-        double res1 = var1c1 / var2c1;
-        double res2 = var1c2 / var2c2;
-        
-        std::cout << "ratio mid component 1: " << res1 << std::endl;
-        std::cout << "ratio mid component 2: " << res2 << std::endl;
-        
-    }
-    
-    // Last node
-    
-    // Check Bulb 2 computation
-    x1_loc = bulb_data.mol_fracs_bulb2.x1;
-    x2_loc = bulb_data.mol_fracs_bulb2.x2;
-    a1_loc = alpha1(p_params, x1_loc, x2_loc);
-    a2_loc = alpha2(p_params, x1_loc, x2_loc);
-    b1_loc = beta1(p_params, x1_loc, x2_loc);
-    b2_loc = beta2(p_params, x1_loc, x2_loc);
-    
-    x1P = tube_fracs[ng - 1].x1;
-    x2P = tube_fracs[ng - 1].x2;
-    
-    double x1e = x1_loc;
-    double x2e = x2_loc;
-    
-    double J1_n = b1_loc * (x1e - x1P) / (0.5 * dz) + b2_loc * (x2e - x2P) / (0.5 * dz);
-    double J2_n = a1_loc * (x1e - x1P) / (0.5 * dz) + a2_loc * (x2e - x2P) / (0.5 * dz);
-    
-    var1c1 = J1_n * A;
-    var2c1 = V * p_params.ct / dt * (bulb_data.mol_fracs_bulb2.x1 - bulb_data_old.mol_fracs_bulb2.x1);
-    
-    var1c2 = J2_n * A;
-    var2c2 = V * p_params.ct / dt * (bulb_data.mol_fracs_bulb2.x2 - bulb_data_old.mol_fracs_bulb2.x2);
-    
-    ratio1 = var1c1 / var2c1;
-    ratio2 = var1c2 / var2c2;
-    
-    std::cout << "ratio 1 bulb 2: " << ratio1 << std::endl;
-    std::cout << "ratio 2 bulb 2: " << ratio2 << std::endl;
-    
-    // Print stuff
-    for(int node = 0; node < ng; ++node) {
-        std::cout << tube_fracs[node].x1 << " ";
-        std::cout << tube_fracs[node].x2 << " ";
-        std::cout << tube_fracs[node].x3;
-        std::cout << std::endl;
-    }
+//    // Check Bulb 1 computation
+//    double x1_loc = bulb_data.mol_fracs_bulb1.x1;
+//    double x2_loc = bulb_data.mol_fracs_bulb1.x2;
+//    double a1_loc = alpha1(p_params, x1_loc, x2_loc);
+//    double a2_loc = alpha2(p_params, x1_loc, x2_loc);
+//    double b1_loc = beta1(p_params, x1_loc, x2_loc);
+//    double b2_loc = beta2(p_params, x1_loc, x2_loc);
+//
+//    double x1P = tube_fracs[0].x1;
+//    double x1W = x1_loc;
+//
+//    double x2P = tube_fracs[0].x2;
+//    double x2W = x2_loc;
+//
+//    double J1_0 = b1_loc * (x1P - x1W) / (0.5 * dz) + b2_loc * (x2P - x2W) / (0.5 * dz);
+//    double J2_0 = a1_loc * (x1P - x1W) / (0.5 * dz) + a2_loc * (x2P - x2W) / (0.5 * dz);
+//
+//    double var1c1 = - J1_0 * A;
+//    double var2c1 = V * p_params.ct / dt * (bulb_data.mol_fracs_bulb1.x1 - bulb_data_old.mol_fracs_bulb1.x1);
+//
+//    double var1c2 = - J2_0 * A;
+//    double var2c2 = V * p_params.ct / dt * (bulb_data.mol_fracs_bulb1.x2 - bulb_data_old.mol_fracs_bulb1.x2);
+//
+//    double ratio1 = var1c1 / var2c1;
+//    double ratio2 = var1c2 / var2c2;
+//
+//    std::cout << "ratio 1 bulb 1: " << ratio1 << std::endl;
+//    std::cout << "ratio 2 bulb 1: " << ratio2 << std::endl;
+//
+//    // Check first node
+//
+//    // Check mid tube nodes
+//    for(int node = 1; node < ng - 1; ++node) {
+//        double x1_loc_z = 0.5 * (tube_fracs[node - 1].x1 + tube_fracs[node].x1);
+//        double x2_loc_z = 0.5 * (tube_fracs[node - 1].x2 + tube_fracs[node].x2);
+//        double a1_loc_z = alpha1(p_params, x1_loc_z, x2_loc_z);
+//        double a2_loc_z = alpha2(p_params, x1_loc_z, x2_loc_z);
+//        double b1_loc_z = beta1(p_params, x1_loc_z, x2_loc_z);
+//        double b2_loc_z = beta2(p_params, x1_loc_z, x2_loc_z);
+//
+//        double x1_loc_z_dz = 0.5 * (tube_fracs[node + 1].x1 + tube_fracs[node].x1);
+//        double x2_loc_z_dz = 0.5 * (tube_fracs[node + 1].x2 + tube_fracs[node].x2);
+//        double a1_loc_z_dz = alpha1(p_params, x1_loc_z_dz, x2_loc_z_dz);
+//        double a2_loc_z_dz = alpha2(p_params, x1_loc_z_dz, x2_loc_z_dz);
+//        double b1_loc_z_dz = beta1(p_params, x1_loc_z_dz, x2_loc_z_dz);
+//        double b2_loc_z_dz = beta2(p_params, x1_loc_z_dz, x2_loc_z_dz);
+//
+//        double x1P = tube_fracs[node].x1;
+//        double x1W = tube_fracs[node - 1].x1;
+//        double x1E = tube_fracs[node + 1].x1;
+//
+//        double x2P = tube_fracs[node].x2;
+//        double x2W = tube_fracs[node - 1].x2;
+//        double x2E = tube_fracs[node + 1].x2;
+//
+//        double J1_z = b1_loc_z * (x1P - x1W) / dz + b2_loc_z * (x2P - x2W) / dz;
+//        double J1_z_dz = b1_loc_z_dz * (x1E - x1P) / dz + b2_loc_z_dz * (x2E - x2P) / dz;
+//        double J2_z = a1_loc_z * (x1P - x1W) / dz + a2_loc_z * (x2P - x2W) / dz;
+//        double J2_z_dz = a1_loc_z_dz * (x1E - x1P) / dz + a2_loc_z_dz * (x2E - x2P) / dz;
+//
+//        double var1c1 = p_params.ct * (tube_fracs[node].x1 - tube_fracs_old[node].x1) / dt;
+//        double var2c1 = (J1_z - J1_z_dz) / dz;
+//
+//        double var1c2 = p_params.ct * (tube_fracs[node].x2 - tube_fracs_old[node].x2) / dt;
+//        double var2c2 = (J2_z - J2_z_dz) / dz;
+//
+//        double res1 = var1c1 / var2c1;
+//        double res2 = var1c2 / var2c2;
+//
+//        std::cout << "ratio mid component 1: " << res1 << std::endl;
+//        std::cout << "ratio mid component 2: " << res2 << std::endl;
+//
+//    }
+//
+//    // Last node
+//
+//    // Check Bulb 2 computation
+//    x1_loc = bulb_data.mol_fracs_bulb2.x1;
+//    x2_loc = bulb_data.mol_fracs_bulb2.x2;
+//    a1_loc = alpha1(p_params, x1_loc, x2_loc);
+//    a2_loc = alpha2(p_params, x1_loc, x2_loc);
+//    b1_loc = beta1(p_params, x1_loc, x2_loc);
+//    b2_loc = beta2(p_params, x1_loc, x2_loc);
+//
+//    x1P = tube_fracs[ng - 1].x1;
+//    x2P = tube_fracs[ng - 1].x2;
+//
+//    double x1e = x1_loc;
+//    double x2e = x2_loc;
+//
+//    double J1_n = b1_loc * (x1e - x1P) / (0.5 * dz) + b2_loc * (x2e - x2P) / (0.5 * dz);
+//    double J2_n = a1_loc * (x1e - x1P) / (0.5 * dz) + a2_loc * (x2e - x2P) / (0.5 * dz);
+//
+//    var1c1 = J1_n * A;
+//    var2c1 = V * p_params.ct / dt * (bulb_data.mol_fracs_bulb2.x1 - bulb_data_old.mol_fracs_bulb2.x1);
+//
+//    var1c2 = J2_n * A;
+//    var2c2 = V * p_params.ct / dt * (bulb_data.mol_fracs_bulb2.x2 - bulb_data_old.mol_fracs_bulb2.x2);
+//
+//    ratio1 = var1c1 / var2c1;
+//    ratio2 = var1c2 / var2c2;
+//
+//    std::cout << "ratio 1 bulb 2: " << ratio1 << std::endl;
+//    std::cout << "ratio 2 bulb 2: " << ratio2 << std::endl;
+//
+//    // Print stuff
+//    for(int node = 0; node < ng; ++node) {
+//        std::cout << tube_fracs[node].x1 << " ";
+//        std::cout << tube_fracs[node].x2 << " ";
+//        std::cout << tube_fracs[node].x3;
+//        std::cout << std::endl;
+//    }
     
     std::cout << "bulb 1 frac 1: " << bulb_data.mol_fracs_bulb1.x1 << std::endl;
     std::cout << "bulb 1 frac 2: " << bulb_data.mol_fracs_bulb1.x2 << std::endl;
@@ -527,26 +559,6 @@ int main(int argc, const char * argv[]) {
     std::cout << "bulb 2 frac 1: " << bulb_data.mol_fracs_bulb2.x1 << std::endl;
     std::cout << "bulb 2 frac 2: " << bulb_data.mol_fracs_bulb2.x2 << std::endl;
     std::cout << "bulb 2 frac 3: " << bulb_data.mol_fracs_bulb2.x3 << std::endl;
-    
-    // Testing
-//    double res1 = 2 * beta1(p_params, 0.1, 0.7) / dz;
-//    double res2 = p_params.ct * dz / dt;
-//    double res3 = 2 * beta2(p_params, 0.1, 0.7) / dz;
-//    double res4 = -beta2(p_params, 0.7, 0.1) / dz;
-//
-//    double res5 = 2 * alpha1(p_params, 0.1, 0.7) / dz;
-//    double res6 = p_params.ct * dz / dt;
-//    double res7 = 2 * alpha2(p_params, 0.1, 0.7) / dz;
-//    double res8 = -alpha2(p_params, 0.7, 0.1) / dz;
-//
-//    std::cout << "2 * beta1 / dz: " << res1 << std::endl;
-//    std::cout << "ct * dz / dt: " << res2 << std::endl;
-//    std::cout << "2 * beta2 / dz: " << res3 << std::endl;
-//    std::cout << "-beta2 / dz: " << res4 << std::endl;
-//
-//    std::cout << "2 * alpha1 / dz: " << res5 << std::endl;
-//    std::cout << "2 * alpha2 / dz: " << res7 << std::endl;
-//    std::cout << "-alpha2 / dz: " << res8 << std::endl;
     
     std::cout << "done" << std::endl;
     
